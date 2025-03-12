2846a1765430f0e8bfaf549c254aa477bd7f8b8e
Tan <nguyenhuytan2004@gmail.com>
2025-03-12 17:15:12 +0700

    add -home- screen

--- src/main/java/com/example/Project/components/home/HomeController.java
+++ src/main/java/com/example/Project/components/home/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.Project.components.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("message", "Hello Handlebars!");
         return "home";
     }
 }
