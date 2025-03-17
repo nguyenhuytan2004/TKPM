deb44491d6e1c43ef050450db84d0c896e1bb804
Tan <nguyenhuytan2004@gmail.com>
2025-03-17 21:44:03 +0700

    add -URL parser- page

---
+++ src/main/java/com/example/Project/controller/web/UrlParserController.java
@@ -0,0 +1,70 @@
+package com.example.Project.controller.web;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Project.model.Category;
+import com.example.Project.service.CategoryService;
+
+@Controller
+@RequestMapping("")
+public class UrlParserController {
+    @Autowired
+    private CategoryService categoriesService;
+
+    @GetMapping("/web/url-parser")
+    public String show(Model model) {
+        List<Category> allCategories = categoriesService.getAllCategories();
+        model.addAttribute("categories", allCategories);
+        return "url-parser";
+    }
+
+    @RestController
+    @RequestMapping("/api/web/url-parser")
+    public class UrlParserHandler {
+        @GetMapping("")
+        public Map<String, Object> parseUrl(String urlString) {
+            Map<String, Object> result = new HashMap<>();
+            try {
+                URI uri = new URI(urlString);
+
+                String userInfo = uri.getUserInfo();
+                String username = "";
+                String password = "";
+                if (userInfo != null && userInfo.contains(":")) {
+                    String[] parts = userInfo.split(":", 2);
+                    username = parts[0];
+                    password = parts[1];
+                } else if (userInfo != null) {
+                    username = userInfo;
+                }
+
+                result.put("protocol", uri.getScheme() != null ? uri.getScheme() : "");
+                result.put("username", username);
+                result.put("password", password);
+                result.put("hostname", uri.getHost() != null ? uri.getHost() : "");
+                result.put("port", uri.getPort() != -1 ? String.valueOf(uri.getPort()) : "");
+                result.put("path", uri.getPath() != null ? uri.getPath() : "");
+                result.put("params", uri.getQuery() != null ? uri.getQuery() : "");
+                result.put("fragment", uri.getFragment() != null ? uri.getFragment() : "");
+
+                result.put("isSuccess", true);
+
+            } catch (URISyntaxException e) {
+                result.put("isSuccess", false);
+
+            }
+            return result;
+        }
+    }
+}
