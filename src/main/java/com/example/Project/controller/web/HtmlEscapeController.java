25f0ff455906cba1261c39d0f3655d65fa2eb99a
Tan <nguyenhuytan2004@gmail.com>
2025-03-17 20:44:42 +0700

    add -HTML Escape- page

---
+++ src/main/java/com/example/Project/controller/web/HtmlEscapeController.java
@@ -0,0 +1,49 @@
+package com.example.Project.controller.web;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Project.model.Category;
+import com.example.Project.service.CategoryService;
+
+@Controller
+@RequestMapping("")
+public class HtmlEscapeController {
+    @Autowired
+    private CategoryService categoriesService;
+
+    @GetMapping("/web/html-escape")
+    public String show(Model model) {
+        List<Category> allCategories = categoriesService.getAllCategories();
+        model.addAttribute("categories", allCategories);
+        return "html-escape";
+    }
+
+    @RestController
+    @RequestMapping("/api/web/html-escape")
+    public class HtmlEscapeHandler {
+        @GetMapping("")
+        public Map<String, String> escapeHtml(@RequestParam String string) {
+            @SuppressWarnings("deprecation")
+            String escapedString = StringEscapeUtils.escapeHtml4(string);
+            return Collections.singletonMap("stringEscaped", escapedString);
+        }
+
+        @GetMapping("/unescape")
+        public Map<String, String> unescapeHtml(@RequestParam String escapedString) {
+            @SuppressWarnings("deprecation")
+            String decodedString = StringEscapeUtils.unescapeHtml4(escapedString);
+            return Collections.singletonMap("stringUnescaped", decodedString);
+        }
+    }
+}
