b4fb56e78755aa38a5066960c8c8d8a3a097769d
xanh <nvxanh75@gmail.com>
2025-04-15 06:08:02 +0700

    delete admin files

--- src/main/java/com/example/Project/repository/IAdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Project.model.Admin;
-
-public interface IAdminRepository extends JpaRepository<Admin, Long> {
-
-}
--- src/main/java/com/example/Project/service/IAdminService.java
+++
@@ -1,5 +0,0 @@
-package com.example.Project.service;
-
-public interface IAdminService {
-
-}
